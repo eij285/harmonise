ba2f3f887b85fd10e674d6273855b2f129b15460
Glitch (harmonise) <none>
2021-08-28 13:22:31 +0000

    👋💂 Checkpoint ./public/js/client.js:35480258/99:35480665/23:35480887/2571 ./public/intro.html:35480665/580

--- public/js/client.js
+++ public/js/client.js
@@ -25,7 +25,7 @@ TrelloPowerUp.initialize({
     var audio = getCurrentAudio();
     audio.play();
     return t.modal({
-      url: '../views/intro.html',
+      url: './intro.html',
       height: 500,
       title: 'Overview of Harmonise'
     });
@@ -58,49 +58,49 @@ TrelloPowerUp.initialize({
         text: "Stage",
         icon: BLACK_ROCKET_ICON,
         callback: placeholder
-      },
-      {
-        text: "a",
-        icon: BLACK_ROCKET_ICON,
-        callback: placeholder
       }
     ]
   },
   
   
   "card-badges": function (t, opts) {
-let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
-    return t
-      .card("name")
-      .get("name")
-      .then(function (cardName) {
-        console.log("We just loaded the card name for fun: " + cardName);
-        return [
-          {
-            // Dynamic badges can have their function rerun
-            // after a set number of seconds defined by refresh.
-            // Minimum of 10 seconds.
-            dynamic: function () {
-              // we could also return a Promise that resolves to
-              // this as well if we needed to do something async first
-              return {
-                text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
-                icon: "./images/icon.svg",
-                color: "green",
-                refresh: 10, // in seconds
-              };
-            },
-          },
-          {
-            // It's best to use static badges unless you need your
-            // badges to refresh.
-            // You can mix and match between static and dynamic
-            text: "Static",
-            icon: HYPERDEV_ICON, // for card front badges only
-            color: null,
-          },
-        ];
-      });
+    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+        return t
+          .card("name")
+          .get("name")
+          .then(function (cardName) {
+          
+            
+            console.log("We just loaded the card name for fun: " + cardName);
+            return [
+              {
+                // Dynamic badges can have their function rerun
+                // after a set number of seconds defined by refresh.
+                // Minimum of 10 seconds.
+                dynamic: function () {
+                  // we could also return a Promise that resolves to
+                  // this as well if we needed to do something async first
+                  return {
+                    text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
+                    icon: "./images/icon.svg",
+                    color: "green",
+                    refresh: 10, // in seconds
+                  };
+                },
+              },
+              {
+                // It's best to use static badges unless you need your
+                // badges to refresh.
+                // You can mix and match between static and dynamic
+                text: "Static",
+                icon: BLACK_ROCKET_ICON, // for card front badges only
+                color: null,
+              },
+            ];
+          
+          
+          
+          });
 
   }
   
