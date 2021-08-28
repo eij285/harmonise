0a5f86355450f65fe80721cf39f5b19b9a371e32
Glitch (harmonise) <none>
2021-08-28 08:29:39 +0000

    🐝🏵 Checkpoint ./public/js/client.js:35480258/319:35480665/150:35480676/2238:35480887/229 ./.glitch-assets:35480676/2648

--- public/js/client.js
+++ public/js/client.js
@@ -5,14 +5,37 @@ var Promise = TrelloPowerUp.Promise;
 var BLACK_ROCKET_ICON =
   "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421";
 
+var audio = new Audio(
+  "https://cdn.glitch.com/914844dd-5f92-437f-b818-848b7cf5b35a%2F7_7.mp3?v=1630136495127"
+);
+
 var audioPlayer = function() {
-  var audio = new Audio(
-    "https://cdn.glitch.com/914844dd-5f92-437f-b818-848b7cf5b35a%2F7_7.mp3?v=1630136495127"
-  );
-  audio.play();
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
 };
 
+
+var audioPlayer2 = function() {
+
+    audio.play();
+
+};
+
+
+
+
 TrelloPowerUp.initialize({
+  "on-enable": function(t, options) {
+    return [
+      {
+        callback: audioPlayer2
+      }
+      
+    ]
+  },
   "card-buttons": function(t, options) {
     return [
       {
@@ -41,4 +64,3 @@ TrelloPowerUp.initialize({
   //   });
   // }
 });
- 
